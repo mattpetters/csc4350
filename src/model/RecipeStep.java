a29d81cc4110df1278a100a81e4c01334b7a2586
pugalug70 <adam.pugliese@gmail.com>
2017-06-30 21:18:39 -0400

    Added getters/setters

--- src/model/RecipeStep.java
+++ src/model/RecipeStep.java
@@ -4,20 +4,33 @@ package model;
  * Created by mattpetters on 6/29/17.
  */
 public class RecipeStep {
-    /**
-     * ID for the recipe step
-     */
-    Integer id;
-    /**
-     * Order number for the recipe step in which it should be completed. Must be >= 1.
-     */
-    Integer orderNumber;
-    /**
-     * Description of the step. String that contains instructions for how to complete the step.
-     */
-    String description;
-    /**
-     * Reference to the recipe that owns this recipe step
-     */
-    Recipe recipe;
+	Integer id; //ID for the recipe step
+    Integer orderNumber; //Order number for the recipe step in which it should be completed. Must be >= 1.
+    String description; //Description of the step. String that contains instructions for how to complete the step.
+    Recipe recipe; //Reference to the recipe that owns this recipe step
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
 }
