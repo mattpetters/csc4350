7561359d6d1c9bc3ae72c0f258cf6545c557137b
pugalug70 <adam.pugliese@gmail.com>
2017-07-04 21:43:04 -0400

    Changed DATE variables to String

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -4,6 +4,7 @@ import java.util.*;
  * Created by mattpetters on 6/29/17.
  */
 
+//none of these variables can have DATE type. SQLite doesn't offer that --Adam
 public class Ingredient {
     Integer id; //ID for the ingredient
     IngredientType type; //Reference to the ingredient type
@@ -11,71 +12,55 @@ public class Ingredient {
     Boolean isMeat; //Boolean that indicates if the ingredient contains meat
     Boolean isFavorite; //Boolean that indicates if the ingredient has been favorited
     Boolean isHidden; //Boolean that indicates if the ingredient has been hidden
-    Date createdAt; //Date that the ingredient was created
+    String createdAt; //Date that the ingredient was created
     String createdBy; //Name of the person who created the ingredient
 	
-    public Integer getId() {
-		return id;
-	}
-    
-	public void setId(Integer id) {
-		this.id = id;
-	}
-    
-    public IngredientType getType() {
-		return type;
-	}
-    
-	public void setType(IngredientType type) {
-		this.type = type;
-	}
-    
-    public String getName() {
-		return name;
-	}
-    
-	public void setName(String name) {
-		this.name = name;
-	}
-    
-	public Boolean getIsMeat() {
-		return isMeat;
-	}
-    
-	public void setIsMeat(Boolean isMeat) {
-		this.isMeat = isMeat;
-	}
-    
-	public Boolean getIsFavorite() {
-		return isFavorite;
-	}
-    
-	public void setIsFavorite(Boolean isFavorite) {
-		this.isFavorite = isFavorite;
-	}
-    
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-    
     public Boolean getIsHidden() {
 		return isHidden;
 	}
-    
 	public void setIsHidden(Boolean isHidden) {
 		this.isHidden = isHidden;
 	}
-    
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
+	public Integer getId() {
+		return id;
 	}
-    
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public IngredientType getType() {
+		return type;
+	}
+	public void setType(IngredientType type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Boolean getIsMeat() {
+		return isMeat;
+	}
+	public void setIsMeat(Boolean isMeat) {
+		this.isMeat = isMeat;
+	}
+	public Boolean getIsFavorite() {
+		return isFavorite;
+	}
+	public void setIsFavorite(Boolean isFavorite) {
+		this.isFavorite = isFavorite;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String string) {
+		this.createdAt = string;
+	}
 	public String getCreatedBy() {
 		return createdBy;
 	}
-    
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
-	}  
+	}    
 }
-
