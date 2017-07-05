c73037a6522bd843baa972a66cb982300ec24ccf
pugalug70 <adam.pugliese@gmail.com>
2017-07-04 21:46:52 -0400

    Added getters/setters

--- src/model/Unit.java
+++ src/model/Unit.java
@@ -3,13 +3,21 @@ package model;
 /**
  * Created by mattpetters on 6/29/17.
  */
+
 public class Unit {
-    /**
-     * ID for the unit
-     */
-    Integer id;
-    /**
-     * Name of the ingredient unit
-     */
-    String name;
+    Integer id; //ID for the unit
+    String name; //Name of the ingredient unit
+	
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
