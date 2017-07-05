fe7b0bf77bdffeb71f21d3d273dcd398f8d23e9e
pugalug70 <adam.pugliese@gmail.com>
2017-07-04 21:47:45 -0400

    Added getters/setters

--- src/model/IngredientType.java
+++ src/model/IngredientType.java
@@ -6,4 +6,17 @@ package model;
 public class IngredientType {
     Integer id;
     String type;
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}    
 }
