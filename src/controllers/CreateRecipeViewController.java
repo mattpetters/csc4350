bf19e997bea4c63ff07f54d4a55d817aebefb9d3
Stephanie <stephaniewyckoff23@gmail.com>
2017-07-16 16:28:00 -0400

    added all to CreateRecipeViewController, need help with tying it all up

--- src/controllers/CreateRecipeViewController.java
+++ src/controllers/CreateRecipeViewController.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
+import javafx.scene.control.Button; 
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
